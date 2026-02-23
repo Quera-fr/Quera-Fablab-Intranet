0c043393af8b9a6cf5bc945ea15cb9c902969d64
Quera <kevin.duranty@quera.fr>
2026-02-23 16:36:54 +0100

    chore: Exclure le dossier tests-e2e de vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 /// <reference types="vitest" />
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
@@ -25,6 +25,7 @@ export default defineConfig(({mode}) => {
       globals: true,
       environment: 'jsdom',
       setupFiles: './src/test/setup.ts',
+      exclude: ['**/node_modules/**', '**/dist/**', 'tests-e2e/**'],
     },
   };
 });
